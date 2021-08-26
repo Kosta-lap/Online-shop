846e2af66f0587a4502fb37a028ecb670b3a8d4c
Muhriddin <55646558+MuhriddinMMM@users.noreply.github.com>
2021-08-26 21:24:13 +0500

    mongoose

--- server.js
+++ server.js
@@ -10,12 +10,35 @@ const shopRoutes = require("./routes/shopRoute");
 
 const app = express();
 
+
+
+
 app.set("views", "templates");
 app.set("view engine", "pug");
 
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, `public`)));
+
+
+
+async function start() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://dbnodejsuser:dbnodejspassword@cluster0.arh6c.mongodb.net/myFirstDatabase",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    app.listen(PORT, () => {
+      console.log("server has been started");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
 app.use(`/auth`, authRoutes);
 app.use(`/admin`, adminRoutes);
